387919d5d2655404af459bee56c00fce73c05ad3
Billy Alexander <billyalexndr@gmail.com>
2024-06-05 17:44:44 +0700

    feat: merge branches

--- src/pages/Dashboard/User/DetailDestination/components/FeedbackInput.jsx
+++ src/pages/Dashboard/User/DetailDestination/components/FeedbackInput.jsx
@@ -145,7 +145,7 @@ function FeedbackInput() {
         </div>
         <button
           type="button"
-          className="w-full mt-2 text-white font-bold text-lg bg-gradient-to-r from-green-600 via-green-500 to-green-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          className="w-full mt-2 text-white font-bold text-lg bg-gradient-to-r from-green-600 via-green-500 to-green-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg px-5 py-2.5 text-center me-2 mb-2"
         >
           SEND
         </button>
